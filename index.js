c09f3fcd92b46a8f9fada8d55af98bb4709136fa
baesso_tabata <tatabaesso@gmail.com>
2020-06-28 14:35:33 -0300

    add: index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', require('./routes'));
+
+app.listen(3000, () => console.log('API is Alive'));
