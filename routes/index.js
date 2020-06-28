7c18f8a698e6e7d2335f907574f94ed2af77adfd
baesso_tabata <tatabaesso@gmail.com>
2020-06-28 14:58:37 -0300

    criando rotas, imagens e iniciando criação das views

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const paypal = require('paypal-rest-sdk');
+const paypalConfig = require('../config/paypal');
+
+paypal.configure(paypalConfig);
+
+const products = require('../config/products');
+
+router.get('/', (req, res) => res.render('index', { products }));
+
+router.post('/buy', (req, res) => {
+    res.send({ success: true }); //Quando cliente clicar pra comprar
+});
+
+router.get('/success', (req, res) => {
+    res.send({ success: true }); //Quando cliente pagar a compra
+});
+
+router.get('/cancel', (req, res) => {
+    res.send({ cancel: true }); //Quando cliente cancelar a compra
+});
