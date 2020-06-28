094780c32c51c3004628b841a68cc2541e063bf6
baesso_tabata <tatabaesso@gmail.com>
2020-06-28 19:17:03 -0300

    adicionando rota para finalizar pagamento

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ const { products } = require('../config/products');
 
 router.get('/', (req, res) => res.render('index', { products }));
 
+let valor = {};
+
 router.post('/buy', (req, res) => {
     const productId = req.query.id;
     const product = products.reduce((all, item) => item.id.toString() === productId ? item : all, {})
@@ -22,7 +24,7 @@ router.post('/buy', (req, res) => {
         "quantity": 1,
     }];
 
-    const valor = { "currency":"BRL", "total":product.preco.toFixed(2)};
+    valor = { "currency":"BRL", "total":product.preco.toFixed(2)};
     const descricao = product.descricao;
 
     const json_pagamento = {
@@ -44,14 +46,33 @@ router.post('/buy', (req, res) => {
             console.warn(err);
         } else {
             pagamento.links.forEach((link) => {
-                if(link.rel === 'approval_url') return res.redirect(link.href);
+                if(link.rel === 'approval_url') return res.redirect(link.href); //armazenar esses dados no banco de dados, pra depois cobrar o cliente do valor do produto e pra trocar a variável global de valor pelo dado inserido no bd
             })
         }
     });
 });
 
 router.get('/success', (req, res) => {
-    res.render('success'); //Quando cliente pagar a compra
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+
+    const execute_payment_json = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": valor
+        }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
+        if(error){
+            console.warn(error.response);
+            throw error;
+        } else {
+            console.log("Pagamento concluído com sucesso!");
+            console.log(JSON.stringify(payment)); //importante inserir esses dados no banco pra constar que o pagamento foi concluído
+            res.render('success'); //Quando cliente pagar a compra
+        }
+    })
 });
 
 router.get('/cancel', (req, res) => {
